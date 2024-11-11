da56236601eb2dac928a6b6f68ff3136f71ee68f
unknown <divinegpc@gmail.com>
2024-11-11 15:40:27 +0100

    Week One Assignment

---
+++ week-1/user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Ask the user for their name, age, and location
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
